8635c3e3362e0e88a424f654d39132fb1d2d9401
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-08-05 20:32:20 +0530

    added node poly fills

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(new NodePolyfillPlugin());
+    return config;
+  },
 };
 
 export default nextConfig;

6751480d15f1c51e83ac448b1d546519adeffa22
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-08-13 23:04:32 +0530

    token selector done

--- src/components/SwapTokenClient.tsx
+++ src/components/SwapTokenClient.tsx
@@ -4,6 +4,8 @@ import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 import { Label } from '@radix-ui/react-dropdown-menu'
 import { Input } from './ui/input'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { Select, SelectContent, SelectTrigger, SelectValue } from '@radix-ui/react-select'
+import { SelectItem } from './ui/select'
 
 const SwapTokenClient = ({ serverTokens } : { 
     serverTokens?: Token[]
@@ -65,6 +67,36 @@ function TokenSelector({
   return (
 
     <>
+    <Select value={selectedToken} onValueChange={onSelectToken}>
+      <SelectTrigger>
+        <SelectValue placeholder= "Select Token">
+          {selected ? (
+            <div>
+              <img src ={selected.logoURI} alt = {selected.name} className='w-6 h-6 rounder-full' loading='lazy' />
+              <span>{selected.symbol}</span>
+            </div>
+          ): 'Select Token'}
+
+        </SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        {tokens.map((t) =>(
+
+          <SelectItem key = {t.address} value= {t.address}>
+            <div className='flex items-center space-x-2'>
+              <img src ={t.logoURI} alt = {t.name} className='w-6 h-6 rounder-full' loading='lazy' />
+              <span>{t.symbol}</span>
+
+            </div>
+
+          </SelectItem>
+        ))}
+      </SelectContent>
+
+
+
+
+    </Select>
     
     </>
   )

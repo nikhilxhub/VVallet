8ad419b91af037d158a1bb588df3864ae9336290
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-08-13 22:23:36 +0530

    tokenSelector

--- src/components/SwapTokenClient.tsx
+++ src/components/SwapTokenClient.tsx
@@ -1,14 +1,75 @@
 import { Token } from '@/types/Token'
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Label } from '@radix-ui/react-dropdown-menu'
+import { Input } from './ui/input'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
 const SwapTokenClient = ({ serverTokens } : { 
     serverTokens?: Token[]
 }) => {
 
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  const [ tokens, setTokens ] = useState<Token[]>(serverTokens ?? []);
+
+  const [ inputAmount, setInputAmount ] = useState<string>("0");
+  const [ inputToken, setInputToken ] = useState<string>('So11111111111111111111111111111111111111112');
+  const [ outputToken, setOutputToken ] = useState<string>('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+
+
+
 
   return (
-    <div>halo</div>
+    
+    <Card>
+        <CardHeader><CardTitle>Jupiter Swap</CardTitle></CardHeader>
+
+        <CardContent>
+          <div>
+            <Label>You Pay</Label>
+            <div>
+
+          <Input 
+            id =  "input-amount"
+            type = "number"
+            value = {inputAmount}
+            onChange={(e) => setInputAmount(e.target.value)}
+            placeholder='0.0'
+            className='flex-grow'
+            />
+            <TokenSelector tokens = {tokens} selectedToken={inputToken} onSelectToken={setInputToken} />
+
+            </div>
+            </div>
+            
+        </CardContent>
+    </Card>
   )
+}
+
+function TokenSelector({
+  tokens,
+  selectedToken,
+  onSelectToken,
+}:{
+  tokens: Token[];
+  selectedToken: string;
+  onSelectToken : (addr: string) => void;
+
+}){
+
+  const selected = tokens.find((t) => t.address == selectedToken);
+
+  return (
+
+    <>
+    
+    </>
+  )
+
+
 }
 
 export default SwapTokenClient
